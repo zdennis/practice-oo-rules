33bbcf3983790c1af026573d009398d8ababe82c
Zach Dennis <zach.dennis@gmail.com>
2012-03-08 22:36:24 -0500

    pulled out converting to array into WordListSanitizer

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -1,8 +1,19 @@
+class WordListSanitizer
+  def sanitize(words)
+    [words].flatten.compact
+  end
+end
+
+
 class WordList
   attr_reader :words
   
+  def initialize
+    @sanitizer = WordListSanitizer.new
+  end
+  
   def words=(words)
-    words = [words].flatten.compact
+    words = @sanitizer.sanitize(words)
     @words = words.map{ |str| 
       str = str.gsub(/[^\w\s\!\"]/, '')
       str = str.gsub(/\s+/, ' ').strip
