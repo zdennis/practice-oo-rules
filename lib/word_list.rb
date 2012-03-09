72bedc4c5a96be3adf06eed7c75e6e4bf7f37559
Zach Dennis <zach.dennis@gmail.com>
2012-03-08 22:47:54 -0500

    Converting WordList to use re-usable and selectable rule implementation

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -1,24 +1,25 @@
+# things aren't shareable yet
+# real object composition would let this work if we wanted to create other
+# classes with similar rules
+
 class WordListSanitizer
-  def sanitize(words)
+  def initialize(words)
     @words = words
-    convert_to_array
-    strip_unallowed_characters
-    remove_unnecessary_whitespace
-    reverse_words_that_start_with_a_vowel
   end
-  
-  protected
   
   def convert_to_array
     @words = [@words].flatten.compact
+    self
   end
   
   def strip_unallowed_characters
     @words = @words.map{ |str| str.gsub(/[^\w\s\!\"]/, '') }
+    self
   end
   
   def remove_unnecessary_whitespace
     @words = @words.map{ |str| str.gsub(/\s+/, ' ').strip }
+    self
   end
   
   def reverse_words_that_start_with_a_vowel
@@ -27,6 +28,11 @@ class WordListSanitizer
         nstr =~ /^[aeiou]/ ? nstr.reverse : nstr
       }.join(" ")
     }
+    self
+  end
+  
+  def and_return_words
+    @words
   end
 end
 
@@ -34,11 +40,16 @@ end
 class WordList
   attr_reader :words
   
-  def initialize(sanitizer=WordListSanitizer.new)
+  def initialize(sanitizer=WordListSanitizer)
     @sanitizer = sanitizer
   end
   
   def words=(words)
-    @words = @sanitizer.sanitize(words)
+    @words = @sanitizer.new(words).
+      convert_to_array.
+      strip_unallowed_characters.
+      remove_unnecessary_whitespace.
+      reverse_words_that_start_with_a_vowel.
+      and_return_words
   end
 end
