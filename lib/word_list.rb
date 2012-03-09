d9e84c5f08971a5e33639f3d8138b0846b974794
Zach Dennis <zach.dennis@gmail.com>
2012-03-08 21:55:34 -0500

    implemented: strips trailing white-space

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -2,6 +2,6 @@ class WordList
   attr_reader :words
   
   def words=(words)
-    @words = words.map{ |str| str.gsub(/\s+/, ' ').lstrip }
+    @words = words.map{ |str| str.gsub(/\s+/, ' ').strip }
   end
 end
