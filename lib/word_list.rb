7ef8d288a4feac39decb6e0b845ba62187c3e3eb
Zach Dennis <zach.dennis@gmail.com>
2012-03-08 21:55:07 -0500

    implemented: strips leading white-space

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -2,6 +2,6 @@ class WordList
   attr_reader :words
   
   def words=(words)
-    @words = words.map{ |str| str.gsub(/\s+/, '') }
+    @words = words.map{ |str| str.gsub(/\s+/, ' ').lstrip }
   end
 end
