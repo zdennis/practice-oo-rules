00695b251a6e3957d0b8c884b97a599630b6a671
Zach Dennis <zach.dennis@gmail.com>
2012-03-08 22:01:47 -0500

    implemented: handles nil as an empty array

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -2,7 +2,7 @@ class WordList
   attr_reader :words
   
   def words=(words)
-    words = [words].flatten
+    words = [words].flatten.compact
     @words = words.map{ |str| 
       str = str.gsub(/\s+/, ' ').strip
       str.split(/\s/).map { |nstr|
--- spec/word_list_spec.rb
+++ spec/word_list_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe WordList do
   describe "#words=" do
+    it "handles nil as an empty array" do
+      subject.words = nil
+      subject.words.should eq([])
+    end
+    
     it "handles a string as a single-word array" do
       subject.words = "hello there"
       subject.words.should eq(["hello there"])
