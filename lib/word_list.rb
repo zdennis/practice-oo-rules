f4caa4f1baa9308ca811f80dcfc840a1eecec36e
Zach Dennis <zach.dennis@gmail.com>
2012-03-09 10:35:26 -0500

    putting all rules in modules as class methods and using a constant to house them on WordList

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -1,55 +1,45 @@
-# things aren't shareable yet
-# real object composition would let this work if we wanted to create other
-# classes with similar rules
 
-class WordListSanitizer
-  def initialize(words)
-    @words = words
+
+module ConvertToArraySanitizer
+  def self.sanitize(words)
+    [words].flatten.compact
   end
-  
-  def convert_to_array
-    @words = [@words].flatten.compact
-    self
+end
+
+module StripUnallowedCharactersSanitizer
+  def self.sanitize(words)
+    words.map{ |str| str.gsub(/[^\w\s\!\"]/, '') }
   end
-  
-  def strip_unallowed_characters
-    @words = @words.map{ |str| str.gsub(/[^\w\s\!\"]/, '') }
-    self
+end
+
+module RemoveUnnecessaryWhitespaceSanitizer
+  def self.sanitize(words)
+    words.map{ |str| str.gsub(/\s+/, ' ').strip }
   end
-  
-  def remove_unnecessary_whitespace
-    @words = @words.map{ |str| str.gsub(/\s+/, ' ').strip }
-    self
-  end
-  
-  def reverse_words_that_start_with_a_vowel
-    @words = @words.map{ |str|
+end
+
+module ReverseWordsThatStartWithVowel
+  def self.sanitize(words)
+    words.map{ |str|
       str = str.split(/\s/).map { |nstr|
         nstr =~ /^[aeiou]/ ? nstr.reverse : nstr
       }.join(" ")
     }
-    self
-  end
-  
-  def and_return_words
-    @words
   end
 end
-
 
 class WordList
   attr_reader :words
   
-  def initialize(sanitizer=WordListSanitizer)
-    @sanitizer = sanitizer
-  end
+  SANITIZERS = [
+    ConvertToArraySanitizer,
+    StripUnallowedCharactersSanitizer,
+    RemoveUnnecessaryWhitespaceSanitizer,
+    ReverseWordsThatStartWithVowel]
   
   def words=(words)
-    @words = @sanitizer.new(words).
-      convert_to_array.
-      strip_unallowed_characters.
-      remove_unnecessary_whitespace.
-      reverse_words_that_start_with_a_vowel.
-      and_return_words
+    @words = SANITIZERS.inject(words) do |words, sanitizer|
+      sanitizer.sanitize words
+    end
   end
 end
