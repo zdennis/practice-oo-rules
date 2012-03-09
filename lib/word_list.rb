5089e6cb8d95af6abb4eb045472e2da401712bd7
Zach Dennis <zach.dennis@gmail.com>
2012-03-08 22:24:37 -0500

    pulled out StripUnallowedCharacters

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -4,17 +4,23 @@ module Rules
       [words].flatten.compact
     end
   end
+  
+  module StripUnallowedCharacters
+    def words=(words)
+      super(words).map{ |str| str.gsub(/[^\w\s\!\"]/, '') }
+    end
+  end
 end
 
 class WordList
   attr_reader :words
   
   include Rules::ConvertToArray
+  include Rules::StripUnallowedCharacters
   
   def words=(words)
     words = super(words)
     @words = words.map{ |str| 
-      str = str.gsub(/[^\w\s\!\"]/, '')
       str = str.gsub(/\s+/, ' ').strip
       str = str.split(/\s/).map { |nstr|
         nstr =~ /^[aeiou]/ ? nstr.reverse : nstr
