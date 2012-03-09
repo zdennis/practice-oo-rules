385c4b4b69a3ea187c12b45e22ac867de8b49f64
Zach Dennis <zach.dennis@gmail.com>
2012-03-08 22:39:02 -0500

    pulled out strip_unallowed_characters into WordListSanitizer

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -1,6 +1,23 @@
 class WordListSanitizer
   def sanitize(words)
-    [words].flatten.compact
+    @words = words
+    convert_to_array
+    strip_unallowed_characters
+    remove_unnecessary_whitespace
+  end
+  
+  protected
+  
+  def convert_to_array
+    @words = [@words].flatten.compact
+  end
+  
+  def strip_unallowed_characters
+    @words = @words.map{ |str| str.gsub(/[^\w\s\!\"]/, '') }
+  end
+  
+  def remove_unnecessary_whitespace
+    @words = @words.map{ |str| str.gsub(/\s+/, ' ').strip }
   end
 end
 
@@ -15,8 +32,6 @@ class WordList
   def words=(words)
     words = @sanitizer.sanitize(words)
     @words = words.map{ |str| 
-      str = str.gsub(/[^\w\s\!\"]/, '')
-      str = str.gsub(/\s+/, ' ').strip
       str = str.split(/\s/).map { |nstr|
         nstr =~ /^[aeiou]/ ? nstr.reverse : nstr
       }.join(" ")
