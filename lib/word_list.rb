1bbbb52708d5af833dbe7856d49ecf510d6fc16b
Zach Dennis <zach.dennis@gmail.com>
2012-03-10 12:32:57 -0500

    updating WordList#words= to internally call well-named protected methods in a functional-style format so @words is only ever assigned once all of the work is done

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -2,13 +2,33 @@ class WordList
   attr_reader :words
   
   def words=(words)
-    words = [words].flatten.compact
-    @words = words.map{ |str| 
-      str = str.gsub(/[^\w\s\!\"]/, '')
-      str = str.gsub(/\s+/, ' ').strip
+    @words = 
+      reverse_words_that_start_with_a_vowel(
+        remove_unnecessary_whitespace(
+          strip_unallowed_characters(
+            convert_to_array(
+              words))))
+  end
+  
+  protected
+  
+  def convert_to_array(words)
+    [words].flatten.compact
+  end
+  
+  def strip_unallowed_characters(words)
+    words.map{ |str| str.gsub(/[^\w\s\!\"]/, '') }
+  end
+  
+  def remove_unnecessary_whitespace(words)
+    words.map{ |str| str.gsub(/\s+/, ' ').strip }
+  end
+  
+  def reverse_words_that_start_with_a_vowel(words)
+    words.map{ |str|
       str = str.split(/\s/).map { |nstr|
         nstr =~ /^[aeiou]/ ? nstr.reverse : nstr
       }.join(" ")
     }
-  end
+  end  
 end
