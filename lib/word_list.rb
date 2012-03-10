0bde013e7b442f81dbbd8c2d04c42b031362eb32
Zach Dennis <zach.dennis@gmail.com>
2012-03-10 12:40:54 -0500

    use many small well-named methods in WordList

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -2,10 +2,29 @@ class WordList
   attr_reader :words
   
   def words=(words)
-    words = [words].flatten.compact
-    @words = words.map{ |str| 
-      str = str.gsub(/[^\w\s\!\"]/, '')
-      str = str.gsub(/\s+/, ' ').strip
+    @words = words
+    convert_to_array
+    strip_unallowed_characters
+    remove_unnecessary_whitespace
+    reverse_words_that_start_with_a_vowel
+  end
+  
+  protected
+  
+  def convert_to_array
+    @words = [@words].flatten.compact
+  end
+  
+  def strip_unallowed_characters
+    @words = @words.map{ |str| str.gsub(/[^\w\s\!\"]/, '') }
+  end
+  
+  def remove_unnecessary_whitespace
+    @words = @words.map{ |str| str.gsub(/\s+/, ' ').strip }
+  end
+  
+  def reverse_words_that_start_with_a_vowel
+    @words = @words.map{ |str|
       str = str.split(/\s/).map { |nstr|
         nstr =~ /^[aeiou]/ ? nstr.reverse : nstr
       }.join(" ")
